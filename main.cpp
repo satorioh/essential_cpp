6259ee09aa1a8ea5ec3b862c8b912b141a1b3c67
Robin.Wang <3Jg$94sbQ@q1>
2024-08-03 18:40:59 +0800

    chore(project): finish chapter 4

--- main.cpp
+++ main.cpp
@@ -1,9 +1,31 @@
-#include "io.h"
+#include <iostream>
 
+double getHeight() {
+    double x{};
+    std::cout << "Enter the height of the tower in meters:";
+    std::cin >> x;
+    return x;
+}
+
+double calcDistance(int second, double height) {
+    const double gravity{9.8};
+    double distance = gravity * second * second / 2;
+    return height - distance;
+}
+
+void loop(double height) {
+    for (int i = 0; i <= 5; i++) {
+        double result{calcDistance(i, height)};
+        if (result <= 0) {
+            std::cout << "At " << i << " seconds, the ball is on the ground.\n";
+        } else {
+            std::cout << "At " << i << " seconds, the ball is at height: " << result << " meters.\n";
+        }
+    }
+}
 
 int main() {
-    int x{readNumber()};
-    int y{readNumber()};
-    writeAnswer(x + y);
+    double height{getHeight()};
+    loop(height);
     return 0;
 }
