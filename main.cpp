6b54632e4ca94ab07967ebea13e99e2d81292eb7
Robin.Wang <3Jg$94sbQ@q1>
2024-08-11 17:03:20 +0800

    chore(project): finish chapter 8

--- main.cpp
+++ main.cpp
@@ -1,16 +1,50 @@
+#include "Random.h"
 #include <iostream>
 
-int accumulate(int x) {
-    static int result{0};
-    result += x;
-    return result;
+void play(int min, int max, int roundLimit) {
+    std::cout <<
+            "Let's play a game. I'm thinking of a number between " << min << " and " << max <<
+            " . You have " << roundLimit << " tries to guess what it is.\n";
+    const int num{Random::get(min, max)};
+    int answer{};
+    int round{1};
+    do {
+        std::cout << "Guess #" << round << ": ";
+        std::cin >> answer;
+        if (answer > num) {
+            std::cout << "Your guess is too high.\n";
+            ++round;
+        } else if (answer < num) {
+            std::cout << "Your guess is too low.\n";
+            ++round;
+        } else {
+            std::cout << "Correct! You win!\n";
+        }
+        if (round > roundLimit) {
+            std::cout << "Sorry, you lose. The correct number was " << num << ".\n";
+            break;
+        }
+    } while (answer != num);
+}
+
+void loop() {
+    std::cout << "Would you like to play again (y/n)? ";
+    std::string yn{};
+    std::cin >> yn;
+    if (yn == "y") {
+        play(1, 100, 5);
+    } else if (yn == "n") {
+        std::cout << "Thank you for playing.";
+    } else {
+        loop();
+    }
 }
 
 int main() {
-    std::cout << accumulate(4) << '\n'; // prints 4
-    std::cout << accumulate(3) << '\n'; // prints 7
-    std::cout << accumulate(2) << '\n'; // prints 9
-    std::cout << accumulate(1) << '\n'; // prints 10
-
+    constexpr int guesses{7}; // the user has this many guesses
+    constexpr int min{1};
+    constexpr int max{100};
+    play(min, max, guesses);
+    loop();
     return 0;
 }
