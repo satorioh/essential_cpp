e0d28713e69f6423adfe7e8e4200ebf3596e230f
Robin.Wang <3Jg$94sbQ@q1>
2024-08-13 15:31:26 +0800

    chore(project): finish chapter 10

--- main.cpp
+++ main.cpp
@@ -1,50 +1,17 @@
-#include "Random.h"
 #include <iostream>
 
-void play(int min, int max, int roundLimit) {
-    std::cout <<
-            "Let's play a game. I'm thinking of a number between " << min << " and " << max <<
-            " . You have " << roundLimit << " tries to guess what it is.\n";
-    const int num{Random::get(min, max)};
-    int answer{};
-    int round{1};
-    do {
-        std::cout << "Guess #" << round << ": ";
-        std::cin >> answer;
-        if (answer > num) {
-            std::cout << "Your guess is too high.\n";
-            ++round;
-        } else if (answer < num) {
-            std::cout << "Your guess is too low.\n";
-            ++round;
-        } else {
-            std::cout << "Correct! You win!\n";
-        }
-        if (round > roundLimit) {
-            std::cout << "Sorry, you lose. The correct number was " << num << ".\n";
-            break;
-        }
-    } while (answer != num);
+int add(int x, int y) {
+    return x + y;
 }
 
-void loop() {
-    std::cout << "Would you like to play again (y/n)? ";
-    std::string yn{};
-    std::cin >> yn;
-    if (yn == "y") {
-        play(1, 100, 5);
-    } else if (yn == "n") {
-        std::cout << "Thank you for playing.";
-    } else {
-        loop();
-    }
+double add(double x, double y) {
+    return x + y;
 }
 
 int main() {
-    constexpr int guesses{7}; // the user has this many guesses
-    constexpr int min{1};
-    constexpr int max{100};
-    play(min, max, guesses);
-    loop();
+    std::cout << add(1, 2); // calls add(int, int)
+    std::cout << '\n';
+    std::cout << add(1.2, 3.4); // calls add(double, double)
+
     return 0;
 }
